4fb49df6677f5b33cd7831c11a3cba25cb5786f5
Brian Atkinson <batkinson@lanl.gov>
2023-06-29 14:26:35 -0600

    Only print NUMA nodes if requested
    After updating the build system to CMake XDD was outputing the NUMA
    domain for each worker thread in the header of the output. This address
    that issue by printing that info in the event it is requested through
    the `-debug` flag.
    
    Since the `GO_DEBUG_USER1` global debug was never used, it was updated
    to be used for init debugging and changed to GO_DEBUG_INIT. So if the
    user does `-debug init` they will now see what cpus the worker threads
    have been pinned to.
    
    As part of the work, I also reorganized the test directory a bit. I made
    a separate debug test directory just to test out this functionality as
    it really did not make sense for it to be in the fuctional test cases. I
    placed all the common test CMake variables in the CMakeLists.txt and
    then just added individual test information in the functional and debug
    CMakeLists.txt files.
    
    Fixes #13
    Signed-off-by: Brian Atkinson <batkinson@lanl.gov>

---
+++ tests/debug/test_xdd_debug_init.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Debug output test for XDD.
+#
+# Validate XDD header has worker thread NUMA cpus printed if requested
+#
+# Description - Just writes out to /dev/null using XDD with -debug INIT and verifies
+#               that the NUMA cpus are listed if requested through the debug flag
+#
+# Source the test configuration environment
+#
+source ../test_config
+source ../common.sh
+
+initialize_test
+
+echo "Foobar and I am here motherfucker"
+
+xdd_cmd="${XDDTEST_XDD_EXE} -op write -reqsize 128 -numreqs 1 -targets 1 /dev/null -verbose -debug INIT"
+${xdd_cmd} 2>&1 | grep "bound to NUMA node"
+
+if [[ $? -ne 0 ]]; then
+  # test failed
+  echo "We are here with $? which is not equal to zero..."
+  finalize_test 1
+fi
+
+xdd_cmd="${XDDTEST_XDD_EXE} -op write -reqsize 128 -numreqs 1 -targets 1 /dev/null -verbose"
+${xdd_cmd} 2>&1 | grep "bound to NUMA node"
+
+if [[ $? -ne 1 ]]; then
+  # test failed
+  finalize_test 1
+fi
+
+# test passed
+finalize_test 0
